f63760f24f2abc9f9b41b7935c2d38ee5ec69333
juan-pablo-web <juangoldenweb@gmail.com>
2022-12-01 18:28:42 -0300

    Date update

--- frontend/src/Components/SalesCard/index.tsx
+++ frontend/src/Components/SalesCard/index.tsx
@@ -16,10 +16,15 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <>
@@ -58,11 +63,13 @@ function SalesCard() {
               </tr>
             </thead>
             <tbody>
-              {sales.map(sale => {
+              {sales.map((sale) => {
                 return (
                   <tr key={sale.id}>
                     <td className="show992">{sale.id}</td>
-                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                    <td className="show576">
+                      {new Date(sale.date).toLocaleDateString()}
+                    </td>
                     <td>{sale.sellerName}</td>
                     <td className="show992">{sale.visited}</td>
                     <td className="show992">{sale.deals}</td>
